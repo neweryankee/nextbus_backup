dd9b2591bf9d134bd4604c65a0cfada2b7aef39b
Greg <gsterndale@gmail.com>
2010-02-18 16:22:00 -0500

    Require Bundler in test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,21 @@
-require 'rubygems'
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 require 'test/unit'
+require 'mocha'
+require 'erb'
+require 'ruby-debug'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'nextbus'
 
 class Test::Unit::TestCase
