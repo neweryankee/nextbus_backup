881ed8e8331bb4ee2725f7164d7304527040624a
Greg <gsterndale@gmail.com>
2010-02-23 09:32:27 -0500

    HTTParty gem

--- lib/nextbus.rb
+++ lib/nextbus.rb
@@ -11,6 +11,8 @@ end
 # Your application requires come here
 require 'instantiate_with_attrs'
 require 'attr_with_default'
+require 'httparty'
+require 'hashie'
 dir = File.join(File.dirname(__FILE__), 'nextbus')
 $LOAD_PATH.unshift(dir)
 Dir[File.join(dir, "*.rb")].each {|file| require File.basename(file) }
